415bc0bf69607c9b8a08d54ef2c9fe03ec2f5d30
forest1040 <forest1040@gmail.com>
2024-10-10 17:46:23 +0900

    modify init.sql

--- operation/db/init.sql
+++ operation/db/init.sql
@@ -1,94 +1,67 @@
-DROP TABLE IF EXISTS main.results;
-DROP TABLE IF EXISTS main.tasks;
+-- DROP TABLE IF EXISTS main.results;
+-- DROP TABLE IF EXISTS main.tasks;
+-- DROP TRIGGER IF EXISTS main.update_tasks_status_trigger;
+DROP TABLE IF EXISTS main.jobs;
 DROP TABLE IF EXISTS main.devices;
 
-DROP TRIGGER IF EXISTS main.update_tasks_status_trigger;
-
+drop table main.devices;
 CREATE TABLE IF NOT EXISTS main.devices (
   id VARCHAR(64) PRIMARY KEY,
-  device_type ENUM ('QPU', 'simulator') NOT NULL,
-  status ENUM ('AVAILABLE', 'NOT_AVAILABLE') DEFAULT 'NOT_AVAILABLE' NOT NULL,
-  restart_at DATETIME,
-  pending_tasks INT DEFAULT 0 NOT NULL,
-  n_qubits INT NOT NULL,
-  n_nodes INT,
+  device_type VARCHAR(32) DEFAULT 'QPU' NOT NULL,
+  status VARCHAR(64) DEFAULT 'available' NOT NULL,
+  available_at DATETIME,
+  pending_jobs INT DEFAULT 0 NOT NULL,
+  n_qubits INT DEFAULT 1 NOT NULL,
   basis_gates VARCHAR(256) NOT NULL,
   instructions VARCHAR(64) NOT NULL,
-  calibration_data TEXT,
+  device_info TEXT,
   calibrated_at DATETIME,
-  description VARCHAR(128) NOT NULL
+  description VARCHAR(128) NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS main.tasks (
-  id VARBINARY(16) PRIMARY KEY,
+drop table main.jobs;
+CREATE TABLE IF NOT EXISTS main.jobs (
+  id VARBINARY(64) PRIMARY KEY,
   owner VARCHAR(64) NOT NULL,
-  name varchar(256),
-  device VARCHAR(64) NOT NULL,
-  n_qubits INT,
-  n_nodes INT,
-  code TEXT NOT NULL,
-  action ENUM ('sampling', 'estimation') NOT NULL,
-  method ENUM ('state_vector', 'sampling'),
-  shots INT,
-  operator VARCHAR(1024),
-  qubit_allocation TEXT,
-  skip_transpilation BOOLEAN DEFAULT false NOT NULL,
-  seed_transpilation INT,
-  seed_simulation INT,
-  n_per_node INT UNSIGNED,
-  simulation_opt text,
-  ro_error_mitigation enum('none', 'pseudo_inverse', 'least_square'),
-  note VARCHAR(1024),
-  status ENUM ('QUEUED', 'QUEUED_FETCHED', 'RUNNING', 'COMPLETED', 'FAILED', 'CANCELLING', 'CANCELLING_FETCHED', 'CANCELLED') NOT NULL DEFAULT 'QUEUED',
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (device) REFERENCES devices(id)
+  name varchar(256) DEFAULT '' NOT NULL,
+  description VARCHAR(1024),
+  device_id VARCHAR(64) NOT NULL,
+  job_detail TEXT,
+  transpiler_info TEXT,
+  simulator_info TEXT,
+  mitigation_info TEXT,
+  job_type VARCHAR(32) DEFAULT 'sampling' NOT NULL,
+  shots INT DEFAULT 1000 NOT NULL,
+  status VARCHAR(32) DEFAULT 'submitted' NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS main.results (
-  task_id VARBINARY(16) PRIMARY KEY,
-  status ENUM('SUCCESS', 'FAILURE', 'CANCELLED') NOT NULL,
-  result TEXT,
-  reason TEXT,
-  transpiled_code TEXT,
-  qubit_allocation TEXT,
-  FOREIGN KEY (task_id) REFERENCES tasks(id) ON DELETE CASCADE
-);
-
-DELIMITER $$
-
-CREATE TRIGGER main.update_tasks_status_trigger
-AFTER INSERT ON main.results
-FOR EACH ROW
-BEGIN
-	IF NEW.status = 'SUCCESS' THEN
-  		UPDATE main.tasks SET status = 'COMPLETED' WHERE id = NEW.task_id;
-  	ELSEIF NEW.status = 'FAILURE' THEN
-  		UPDATE main.tasks SET status = 'FAILED' WHERE id = NEW.task_id;
-  	ELSEIF NEW.status = 'CANCELLED' THEN
-  		UPDATE main.tasks SET status = 'CANCELLED' WHERE id = NEW.task_id;
-  	END IF;
-END$$
-
-DELIMITER ;
-
-# Insert devices
-INSERT INTO main.devices (id, device_type, n_qubits, n_nodes, basis_gates, instructions, description)
-SELECT 'SC', 'QPU', 64, NULL, '["sx", "rz", "rzx90", "id"]', '["measure", "barrier"]', 'Superconducting quantum computer'
+-- Insert devices
+INSERT INTO main.devices (id, device_type, status, available_at, pending_jobs, n_qubits, basis_gates, instructions, device_info, calibrated_at, description)
+SELECT 'SC', 'QPU','available', CURRENT_TIMESTAMP, 9, 64, '["sx", "rz", "rzx90", "id"]', '["measure", "barrier"]', '', CURRENT_TIMESTAMP, 'Superconducting quantum computer'
 WHERE NOT EXISTS (SELECT * FROM main.devices WHERE id = 'SC');
 
-INSERT INTO main.devices (id, device_type, n_qubits, n_nodes, basis_gates, instructions, description)
-SELECT 'SVSim', 'simulator', 39, 512, '["x", "y", "z", "h", "s", "sdg", "t", "tdg", "rx", "ry", "rz", "cx", "cz", "swap", "u1", "u2", "u3", "u", "p", "id", "sx", "sxdg"]', '["measure", "barrier", "reset"]', 'State vector-based quantum circuit simulator'
+INSERT INTO main.devices (id, device_type, status, available_at, pending_jobs, n_qubits, basis_gates, instructions, device_info, calibrated_at, description)
+SELECT 'SVSim', 'simulator','available', CURRENT_TIMESTAMP, 0, 39, '["x", "y", "z", "h", "s", "sdg", "t", "tdg", "rx", "ry", "rz", "cx", "cz", "swap", "u1", "u2", "u3", "u", "p", "id", "sx", "sxdg"]', '["measure", "barrier", "reset"]', '', CURRENT_TIMESTAMP, 'State vector-based quantum circuit simulator'
 WHERE NOT EXISTS (SELECT * FROM main.devices WHERE id = 'SVSim');
 
-INSERT INTO main.devices (id, device_type, n_qubits, n_nodes, basis_gates, instructions, description)
-SELECT 'Kawasaki', 'QPU', 64, NULL, '["sx", "rz", "rzx90", "id"]', '["measure", "barrier"]', 'Superconducting quantum computer'
+INSERT INTO main.devices (id, device_type, status, available_at, pending_jobs, n_qubits, basis_gates, instructions, device_info, calibrated_at, description)
+SELECT 'Kawasaki', 'QPU','available', CURRENT_TIMESTAMP, 2, 64, '["sx", "rz", "rzx90", "id"]', '["measure", "barrier"]', '', CURRENT_TIMESTAMP, 'Superconducting quantum computer'
 WHERE NOT EXISTS (SELECT * FROM main.devices WHERE id = 'Kawasaki');
 
-INSERT INTO main.devices (id, device_type, n_qubits, n_nodes, basis_gates, instructions, description)
-SELECT 'test', 'QPU', 64, NULL, '["sx", "rz", "rzx90", "id"]', '["measure", "barrier"]', 'Superconducting quantum computer'
-WHERE NOT EXISTS (SELECT * FROM main.devices WHERE id = 'test');
+INSERT INTO main.devices (id, device_type, status, available_at, pending_jobs, n_qubits, basis_gates, instructions, device_info, calibrated_at, description)
+SELECT '01927422-86d4-7597-b724-b08a5e7781fc', 'QPU','unavailable', CURRENT_TIMESTAMP, 0, 64, '["sx", "rz", "rzx90", "id"]', '["measure", "barrier"]', '', CURRENT_TIMESTAMP, 'Superconducting quantum computer'
+WHERE NOT EXISTS (SELECT * FROM main.devices WHERE id = '01927422-86d4-7597-b724-b08a5e7781fc');
 
-# Insert tasks
-INSERT INTO main.tasks (id, owner, name, device, n_qubits, n_nodes, code, action, method, shots, operator, qubit_allocation, skip_transpilation, seed_transpilation, seed_simulation, n_per_node, simulation_opt, ro_error_mitigation, note, status, created_at)
-SELECT UNHEX(REPLACE('7af020f6-2e38-4d70-8cf0-4349650ea08c', '-', '')), 'admin', 'Test task', 'Kawasaki', 2, 12, 'OPENQASM 2.0;\ninclude "qelib1.inc";\nqreg q[2];\nh q[0];\ncx q[0], q[1];\nmeasure q[0] -> c[0];\nmeasure q[1] -> c[1];', 'sampling', 'state_vector', 1024, 'Z0*Z1', NULL, false, NULL, NULL, 1, NULL, 'none', NULL, 'QUEUED', CURRENT_TIMESTAMP
-WHERE NOT EXISTS (SELECT * FROM main.tasks WHERE owner = 'admin' AND name = 'Test task');
+-- Insert jobs
+INSERT INTO main.jobs (id, owner, name, description, device_id, job_detail, transpiler_info, simulator_info, mitigation_info, job_type, shots, status)
+SELECT '01927422-86d4-73d6-abb4-f2de6a4f5910', 'admin', 'Test job 1', 'Test job 1 description', 'Kawasaki', '{\'code\': \'todo\'}', '', '', '', 'sampling', 1000, 'submitted'
+WHERE NOT EXISTS (SELECT * FROM main.jobs WHERE owner = 'admin' AND name = 'Test job 1');
+
+INSERT INTO main.jobs (id, owner, name, description, device_id, job_detail, transpiler_info, simulator_info, mitigation_info, job_type, shots, status)
+SELECT '01927422-86d4-7cbf-98d3-32f5f1263cd9', 'admin', 'Test job 2', 'Test job 2 description', 'Kawasaki', '{\'code\': \'todo\'}', '', '', '', 'sampling', 1000, 'submitted'
+WHERE NOT EXISTS (SELECT * FROM main.jobs WHERE owner = 'admin' AND name = 'Test job 2');
+
