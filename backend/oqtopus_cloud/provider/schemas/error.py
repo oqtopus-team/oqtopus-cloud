bf3f2219fa963e3530c1a7b066dd2addd0048aa5
shgokita <gokita.shun@fujitsu.com>
2025-01-21 10:03:08 +0900

    chore: replace 'detail' to 'message' in OAS

--- backend/oqtopus_cloud/provider/schemas/error.py
+++ backend/oqtopus_cloud/provider/schemas/error.py
@@ -8,16 +8,16 @@ from pydantic import BaseModel
 
 
 class BadRequest(BaseModel):
-    detail: str
+    message: str
 
 
 class NotFoundError(BaseModel):
-    detail: str
+    message: str
 
 
 class InternalServerError(BaseModel):
-    detail: str
+    message: str
 
 
 class ConflictError(BaseModel):
-    detail: str
+    message: str
