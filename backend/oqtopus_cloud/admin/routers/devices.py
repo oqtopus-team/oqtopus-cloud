5e3c97819e440d19e854db309bc37050e9498925
12219 <matsumoto.keita@sec.co.jp>
2025-02-06 12:59:45 +0900

    feat: add device management endpoints and schemas for device information

---
+++ backend/oqtopus_cloud/admin/routers/devices.py
@@ -0,0 +1,229 @@
+import json
+
+from fastapi import APIRouter, Body, Depends, status
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from oqtopus_cloud.admin.conf import logger, tracer
+from oqtopus_cloud.admin.schemas.devices import (
+    DeviceBase,
+    DeviceInfo,
+)
+from oqtopus_cloud.admin.schemas.errors import (
+    BadRequestErrorResponse,
+    InternalServerErrorResponse,
+    Message,
+    NotFoundErrorResponse,
+)
+from oqtopus_cloud.admin.schemas.success import SuccessResponse
+from oqtopus_cloud.common.models.device import Device
+from oqtopus_cloud.common.session import (
+    get_db,
+)
+
+from . import LoggerRouteHandler
+
+router: APIRouter = APIRouter(route_class=LoggerRouteHandler)
+
+
+@router.get(
+    "/devices", response_model=list[DeviceInfo], responses={500: {"model": Message}}
+)
+@tracer.capture_method
+def get_devices(
+    db: Session = Depends(get_db),
+) -> list[DeviceInfo] | InternalServerErrorResponse:
+    try:
+        logger.info("invoked list_devices")
+        devices = db.scalars(select(Device)).all()
+        return [model_to_schema(device) for device in devices]
+    except Exception as e:
+        logger.error(f"error: {str(e)}", stack_info=True)
+        return InternalServerErrorResponse(message=str(e))
+
+
+@router.get(
+    "/devices/{device_id}",
+    response_model=DeviceInfo,
+    responses={404: {"model": Message}, 500: {"model": Message}},
+)
+@tracer.capture_method
+def get_device(
+    device_id: str,
+    db: Session = Depends(get_db),
+) -> DeviceInfo | NotFoundErrorResponse | InternalServerErrorResponse:
+    """_summary_
+
+    Args:
+        device_id (str): _description_
+        db (Session, optional): _description_. Defaults to Depends(get_db).
+
+    Returns:
+        GetDeviceResponse: _description_
+    """
+    try:
+        device = db.scalars(select(Device).where(Device.id == device_id)).first()
+        logger.info("invoked get_device")
+        if device:
+            response = model_to_schema(device)
+            return response
+        else:
+            message = f"device_id={device_id} is not found."
+            logger.info(message)
+            return NotFoundErrorResponse(message=message)
+    except Exception as e:
+        logger.error(f"error: {str(e)}", stack_info=True)
+        return InternalServerErrorResponse(message=str(e))
+
+
+@router.post(
+    "/devices",
+    response_model=SuccessResponse,
+    responses={400: {"model": Message}, 500: {"model": Message}},
+)
+@tracer.capture_method
+def register_devices(
+    device_info: DeviceBase = Body(..., description="device information"),
+    db: Session = Depends(get_db),
+) -> SuccessResponse | BadRequestErrorResponse | InternalServerErrorResponse:
+    try:
+        logger.info("invoked register_device")
+        device_id = check_device_id(device_info)
+        if device_id is None:
+            return BadRequestErrorResponse(message="device_id is required")
+        existing_device = db.scalars(
+            select(Device).where(Device.id == device_id)
+        ).first()
+        if existing_device:
+            return BadRequestErrorResponse(
+                message=f"device_id={device_id} already exists"
+            )
+        new_device = schema_to_model(device_id, device_info)
+        db.add(new_device)
+        db.commit()
+        return SuccessResponse(message="Device registered successfully")
+    except Exception as e:
+        logger.error(f"error: {str(e)}", stack_info=True)
+        return InternalServerErrorResponse(message=str(e))
+
+
+@router.patch(
+    "/devices/{device_id}",
+    response_model=SuccessResponse,
+    responses={
+        404: {"model": Message},
+        500: {"model": Message},
+    },
+)
+@tracer.capture_method
+def update_device_data(
+    device_id: str,
+    device_update: DeviceBase = Body(..., description="new status"),
+    db: Session = Depends(get_db),
+) -> SuccessResponse | NotFoundErrorResponse | InternalServerErrorResponse:
+    try:
+        logger.info("invoked update device data")
+        # query
+        stmt = select(Device).where(Device.id == device_id)
+        query = db.execute(stmt).scalars().first()
+        if not query:
+            return NotFoundErrorResponse(message="Device not found")
+        update_fields = device_update.model_dump(exclude_none=True)
+        for field, value in update_fields.items():
+            setattr(query, field, value)
+        # commit the transaction
+        db.commit()
+        # refresh the object to get the updated value
+        return SuccessResponse(message="Device updated successfully")
+    except Exception as e:
+        tracer.put_annotation("db_error", str(e))
+        return InternalServerErrorResponse(message="Internal Server Error")
+
+
+@router.delete(
+    "/devices/{device_id}",
+    response_model=None,
+    status_code=status.HTTP_204_NO_CONTENT,
+    responses={
+        404: {"model": Message},
+        500: {"model": Message},
+    },
+)
+@tracer.capture_method
+def delete_device(
+    device_id: str,
+    db: Session = Depends(get_db),
+) -> SuccessResponse | NotFoundErrorResponse | InternalServerErrorResponse:
+    try:
+        logger.info("invoked delete device")
+        # query
+        stmt = select(Device).where(Device.id == device_id)
+        # pageination
+        query_result = db.execute(stmt).scalars().first()
+        if not query_result:
+            return NotFoundErrorResponse(message="Device not found")
+        # delete from RDS
+        db.delete(query_result)
+        db.commit()
+        return SuccessResponse(message="Device deleted successfully")
+    except Exception as e:
+        tracer.put_annotation("db_error", str(e))
+        return InternalServerErrorResponse(message="Internal Server Error")
+
+
+def check_device_id(device_base: DeviceBase) -> str | None:
+    try:
+        if device_base.device_info is None:
+            return None
+        device_info = json.loads(device_base.device_info)
+        return device_info.get("device_id", None)
+    except Exception:
+        return None
+
+
+def model_to_schema_changeable(model: Device) -> DeviceBase:
+    dict = {
+        "device_type": getattr(model, "device_type", None),
+        "status": model.status,
+        "n_qubits": getattr(model, "n_qubits", None),
+        "available_at": getattr(model, "available_at", None),
+        "basis_gates": json.loads(getattr(model, "basis_gates", "[]")),
+        "supported_instructions": json.loads(getattr(model, "instructions", "[]")),
+        "device_info": getattr(model, "device_info", None),
+        "calibrated_at": getattr(model, "calibrated_at", None),
+        "description": model.description,
+    }
+    return DeviceBase.model_validate(dict)
+
+
+def model_to_schema(model: Device) -> DeviceInfo:
+    dict = {
+        "device_id": getattr(model, "id", None),
+        "device_type": getattr(model, "device_type", None),
+        "status": model.status,
+        "available_at": getattr(model, "available_at", None),
+        "n_pending_jobs": getattr(model, "pending_jobs", None),
+        "n_qubits": getattr(model, "n_qubits", None),
+        "basis_gates": json.loads(getattr(model, "basis_gates", "[]")),
+        "supported_instructions": json.loads(getattr(model, "instructions", "[]")),
+        "device_info": getattr(model, "device_info", None),
+        "calibrated_at": getattr(model, "calibrated_at", None),
+        "description": model.description,
+    }
+    return DeviceInfo.model_validate(dict)
+
+
+def schema_to_model(device_id: str, schema: DeviceBase) -> Device:
+    model = Device(
+        id=device_id,
+        device_type=schema.device_type,
+        status=schema.status,
+        available_at=schema.available_at,
+        n_qubits=schema.n_qubits,
+        basis_gates=json.dumps(schema.basis_gates),
+        instructions=json.dumps(schema.supported_instructions),
+        device_info=schema.device_info,
+        calibrated_at=schema.calibrated_at,
+        description=schema.description,
+    )
+    return model
---
+++ backend/tests/oqtopus_cloud/admin/routers/test_devices.py
@@ -0,0 +1,230 @@
+import json
+import logging
+from datetime import datetime
+
+from fastapi.testclient import TestClient
+from oqtopus_cloud.admin.lambda_function import app
+from oqtopus_cloud.admin.schemas.devices import (
+    DeviceBase,
+    DeviceInfo,
+    DeviceType,
+    Status,
+)
+from oqtopus_cloud.common.models.device import Device
+from pydantic.type_adapter import TypeAdapter
+
+logger = logging.getLogger(__name__)
+
+client = TestClient(app)
+
+
+def _get_model(n, device_info={}):
+    mode_dict = {
+        "id": f"SVSim{n}",
+        "device_type": "simulator",
+        "status": "available",
+        "available_at": datetime(2023, 1, 2, 12, 34, 56),
+        "pending_jobs": n,
+        "n_qubits": 1 + n,
+        "basis_gates": '["x", "sx", "rz", "cx"]',
+        "instructions": '["measure", "barrier", "reset"]',
+        "device_info": json.dumps(device_info),
+        "calibrated_at": datetime(2024, 3, 4, 12, 34, 56),
+        "description": "State vector-based quantum circuit simulator",
+        "created_at": datetime(2024, 3, 4, 12, 34, 56),
+    }
+    return Device(**mode_dict)
+
+
+def test_get_devices(
+    test_db,
+):
+    """_summary_
+    Simple GET /devices tests
+    """
+    device_info = {"device_id": "SVSim1"}
+
+    test_db.flush()
+    test_db.add(_get_model(1, device_info))
+    test_db.add(_get_model(2, device_info))
+    test_db.commit()
+    response = client.get("/devices")
+    adapter = TypeAdapter(list[DeviceInfo])
+    actual = adapter.validate_python(response.json())
+    expected = [
+        DeviceInfo(
+            device_id="SVSim1",
+            device_type=DeviceType.simulator,
+            status=Status.available,
+            available_at=datetime(2023, 1, 2, 12, 34, 56),
+            n_pending_jobs=1,
+            n_qubits=2,
+            basis_gates=["x", "sx", "rz", "cx"],
+            supported_instructions=["measure", "barrier", "reset"],
+            device_info=json.dumps(device_info),
+            calibrated_at=datetime(2024, 3, 4, 12, 34, 56),
+            description="State vector-based quantum circuit simulator",
+        ),
+        DeviceInfo(
+            device_id="SVSim2",
+            device_type=DeviceType.simulator,
+            status=Status.available,
+            available_at=datetime(2023, 1, 2, 12, 34, 56),
+            n_pending_jobs=2,
+            n_qubits=3,
+            basis_gates=["x", "sx", "rz", "cx"],
+            supported_instructions=["measure", "barrier", "reset"],
+            device_info=json.dumps(device_info),
+            calibrated_at=datetime(2024, 3, 4, 12, 34, 56),
+            description="State vector-based quantum circuit simulator",
+        ),
+    ]
+    assert response.status_code == 200
+    assert actual == expected
+
+
+def test_get_device(
+    test_db,
+):
+    """_summary_
+    Simple GET /devices/{device_id} tests
+    """
+    device_info = {"device_id": "SVSim1"}
+
+    test_db.flush()
+    test_db.add(_get_model(1, device_info))
+    test_db.commit()
+    response = client.get("/devices/SVSim1")
+    adapter = TypeAdapter(DeviceInfo)
+    actual = adapter.validate_python(response.json())
+    expected = DeviceInfo(
+        device_id="SVSim1",
+        device_type=DeviceType.simulator,
+        status=Status.available,
+        available_at=datetime(2023, 1, 2, 12, 34, 56),
+        n_pending_jobs=1,
+        n_qubits=2,
+        basis_gates=["x", "sx", "rz", "cx"],
+        supported_instructions=["measure", "barrier", "reset"],
+        device_info=json.dumps(device_info),
+        calibrated_at=datetime(2024, 3, 4, 12, 34, 56),
+        description="State vector-based quantum circuit simulator",
+    )
+    assert response.status_code == 200
+    assert actual == expected
+
+
+def test_get_device_no_device(
+    test_db,
+):
+    """_summary_
+    Simple GET /devices/{device_id} tests with no device
+    """
+    device_info = {"device_id": "SVSim1"}
+
+    test_db.flush()
+    test_db.add(_get_model(1, device_info))
+    test_db.commit()
+    response = client.get("/devices/SVSim2")
+    assert response.status_code == 404
+
+
+def test_register_devices(
+    test_db,
+):
+    """_summary_
+    Simple POST /devices tests
+    """
+    device_info = {"device_id": "SVSim1"}
+
+    body = {
+        "device_info": json.dumps(device_info),
+        "device_type": "simulator",
+        "status": "available",
+        "n_qubits": 2,
+        "available_at": "2023-01-02T12:34:56",
+        "basis_gates": '["x", "sx", "rz", "cx", "t"]',
+        "supported_instructions": '["measure", "barrier", "reset"]',
+        "calibrated_at": "2024-03-04T12:34:56",
+        "description": "State vector-based quantum circuit simulator",
+    }
+
+    response = client.post("/devices", json=body)
+    assert response.status_code == 200
+    assert response.json() == {"message": "Device registered successfully"}
+    # confirm the device is registered
+    device = test_db.query(Device).filter(Device.id == "SVSim1").first()
+    assert device.basis_gates == '"[\\"x\\", \\"sx\\", \\"rz\\", \\"cx\\", \\"t\\"]"'
+
+
+def test_register_devices_overlap(
+    test_db,
+):
+    """_summary_
+    Simple POST /devices tests with overlapping device_id
+    """
+    device_info = {"device_id": "SVSim1"}
+
+    test_db.flush()
+    test_db.add(_get_model(1, device_info))
+    test_db.commit()
+    body = {
+        "device_info": json.dumps(device_info),
+        "device_type": "simulator",
+        "status": "available",
+        "n_qubits": 2,
+        "available_at": "2023-01-02T12:34:56",
+        "basis_gates": '["x", "sx", "rz", "cx"]',
+        "supported_instructions": '["measure", "barrier", "reset"]',
+        "calibrated_at": "2024-03-04T12:34:56",
+        "description": "State vector-based quantum circuit simulator",
+    }
+
+    response = client.post("/devices", json=body)
+    assert response.status_code == 400
+    assert response.json() == {"message": "device_id=SVSim1 already exists"}
+
+
+def test_update_devices_full(
+    test_db,
+):
+    """_summary_
+    Simple PATCH /devices/{device_id} tests
+    """
+
+    device_info = {"device_id": "SVSim1"}
+
+    test_db.flush()
+    test_db.add(_get_model(1, device_info))
+    test_db.commit()
+    body = {
+        "device_info": json.dumps(device_info),
+        "device_type": "simulator",
+        "status": "available",
+        "n_qubits": 2,
+        "available_at": "2023-01-02T12:34:56",
+        "basis_gates": '["x", "sx", "rz", "cx"]',
+        "supported_instructions": '["measure", "barrier", "reset"]',
+        "calibrated_at": "2024-03-04T12:34:56",
+        "description": "State vector-based quantum circuit simulator",
+    }
+    response = client.patch("/devices/SVSim1", json=body)
+    assert response.status_code == 200
+    assert response.json() == {"message": "Device updated successfully"}
+
+
+def test_delete_devices(
+    test_db,
+):
+    """_summary_
+    Simple DELETE /devices/{device_id} tests
+    """
+    device_info = {"device_id": "SVSim1"}
+    test_db.flush()
+    test_db.add(_get_model(1, device_info))
+    test_db.commit()
+    response = client.delete("/devices/SVSim1")
+    assert response.status_code == 204
+    # confirm the device is deleted
+    device = test_db.query(Device).filter(Device.id == "SVSim1").first()
+    assert device is None
