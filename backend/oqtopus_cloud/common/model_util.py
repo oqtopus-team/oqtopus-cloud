3cce96919b8407c891bf040c251c18724c2aa9a4
Katsujukou Kineya <74475348+katsujukou@users.noreply.github.com>
2025-06-03 17:08:22 +0900

    fix: fix devices model to respect timezone (#230)

--- backend/oqtopus_cloud/common/model_util.py
+++ backend/oqtopus_cloud/common/model_util.py
@@ -1,4 +1,7 @@
+import datetime
 from typing import Any, Dict
+
+from sqlalchemy.types import DateTime, TypeDecorator
 
 
 def model_to_dict(model: Any) -> Dict[Any, Any]:
@@ -16,3 +19,20 @@ def model_to_schema_dict(
     for model_field, schema_field in map_model_to_schema.items():
         schema_dict[schema_field] = model_dict[model_field]
     return schema_dict
+
+
+class DateTimeTz(TypeDecorator):
+    impl = DateTime
+    cache_ok = True
+
+    def process_bind_param(self, value, dialect):
+        # When storing data to DB, we change the timezone to UTC
+        if value is not None and value.tzinfo is not None:
+            return value.astimezone(datetime.timezone.utc).replace(tzinfo=None)
+        return value
+
+    def process_result_value(self, value, dialect):
+        # When retrieving data from DB, we add the timezone info UTC
+        if value is not None:
+            return value.replace(tzinfo=datetime.timezone.utc)
+        return value
