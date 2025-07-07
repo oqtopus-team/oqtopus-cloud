375045ae1412ca0814b8f7240adc129836223228
dvlpwork (Yugo Matsumoto) <dvlpwork@gmail.com>
2025-07-07 20:36:47 +0900

    chore: update logic

--- scripts/build_slack_notification.py
+++ scripts/build_slack_notification.py
@@ -8,6 +8,7 @@ parser.add_argument("--success", type=int)
 parser.add_argument("--timestamp", type=str)
 parser.add_argument("--run-url", type=str)
 parser.add_argument("--deploy-target", type=str)
+parser.add_argument("--message", type=str, default="")
 args = parser.parse_args()
 
 slack_token = os.environ.get("SLACK_API_TOKEN")
@@ -15,6 +16,12 @@ client = WebClient(token=slack_token)
 
 if args.success:
     text = f"✅ Deploy successful {args.deploy_target}"
+    if args.message == "":
+        message = "Deployed successfully!"
+    else:
+        message = f"{args.message}"
+        message = message[:2000]
+
     attachments = [
         {
             "color": "#00dd00",
@@ -30,7 +37,7 @@ if args.success:
                     "type": "section",
                     "text": {
                         "type": "mrkdwn",
-                        "text": f"[{args.timestamp}] Deployed successfully!",
+                        "text": f"[{args.timestamp}] {message}",
                     },
                 },
                 {
@@ -56,6 +63,12 @@ if args.success:
 
 else:
     text = f"❌ Deploy failed {args.deploy_target}"
+    if args.message == "":
+        message = "Deploy failed!"
+    else:
+        message = f"{args.message}"
+        message = message[:2000]
+
     attachments = [
         {
             "color": "#dd0000",
@@ -71,7 +84,7 @@ else:
                     "type": "section",
                     "text": {
                         "type": "mrkdwn",
-                        "text": f"[{args.timestamp}] Deploy failed!",
+                        "text": f"[{args.timestamp}] {message}",
                     },
                 },
                 {
