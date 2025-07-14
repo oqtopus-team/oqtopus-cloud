b702d3caf96419544eecad59bf9afd022b2d2cbc
Naoyuki Masumoto <masumoto.naoyuki.qiqb@osaka-u.ac.jp>
2025-07-14 18:27:50 +0900

    feat(infra): add aws account id output

--- terraform/infrastructure/oqtopus-dev/main.tf
+++ terraform/infrastructure/oqtopus-dev/main.tf
@@ -1,3 +1,8 @@
+data "aws_caller_identity" "current" {}
+
+output "aws_account_id" {
+  value = data.aws_caller_identity.current.account_id
+}
 
 module "network" {
   source   = "../modules/network"
