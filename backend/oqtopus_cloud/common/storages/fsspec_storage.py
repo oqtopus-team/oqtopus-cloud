2e1d27a2045321a80b65bcbaf3269dccb4206ede
GoralczykS <Slawomir.Goralczyk@fujitsu.com>
2025-08-06 16:13:09 +0200

    bugfix: replaced usgage of os.path.relpath() with standard string operations

--- backend/oqtopus_cloud/common/storages/fsspec_storage.py
+++ backend/oqtopus_cloud/common/storages/fsspec_storage.py
@@ -60,10 +60,11 @@ class FSSpecStorage(AbstractStorage):
 
     def prefix(self, prefix: str) -> Iterator[str]:
         full_prefix = f"{self.fs_url}/{prefix}".rstrip("/")
+        storage_base = cast(str, self.fs._strip_protocol(self.fs_url))
+
         for dirpath, _, filenames in self.fs.walk(full_prefix):
             for filename in filenames:
                 full_path = f"{dirpath}/{filename}"
-                relative_path = os.path.relpath(
-                    full_path, cast(str, self.fs._strip_protocol(self.fs_url))
-                )
-                yield relative_path
+                if (full_path.startswith(storage_base)):
+                   relative_path = full_path[len(storage_base):].lstrip("/")
+                   yield relative_path
