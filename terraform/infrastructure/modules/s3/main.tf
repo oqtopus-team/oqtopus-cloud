0ab35b59dcccc2f6c387ee1fd6ebc74b141486ed
GoralczykS <Slawomir.Goralczyk@fujitsu.com>
2025-12-15 14:35:06 +0100

    feat: enforced HTTPS access

--- terraform/infrastructure/modules/s3/main.tf
+++ terraform/infrastructure/modules/s3/main.tf
@@ -26,3 +26,28 @@ resource "aws_s3_bucket" "this" {
     Name = "${var.product}-${var.org}-${var.env}"
   }
 }
+
+resource "aws_s3_bucket_policy" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "EnforceHTTPSAccess"
+        Effect    = "Deny"
+        Principal = "*"
+        Action    = "s3:*"
+        Resource = [
+          aws_s3_bucket.this.arn,
+          "${aws_s3_bucket.this.arn}/*"
+        ]
+        Condition = {
+          Bool = {
+            "aws:SecureTransport" = "false"
+          }
+        }
+      }
+    ]
+  })
+}
