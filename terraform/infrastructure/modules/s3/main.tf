d4cc563e8ac96892b0f27b65bd1a3cc519147c84
GoralczykS <Slawomir.Goralczyk@fujitsu.com>
2025-12-16 09:56:28 +0100

    feat: explicitly enabled bucket encryption and block public access

--- terraform/infrastructure/modules/s3/main.tf
+++ terraform/infrastructure/modules/s3/main.tf
@@ -27,6 +27,25 @@ resource "aws_s3_bucket" "this" {
   }
 }
 
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource "aws_s3_bucket_policy" "this" {
   bucket = aws_s3_bucket.this.id
 
@@ -58,6 +77,25 @@ resource "aws_s3_bucket" "logs" {
   tags = {
     Name = "${var.product}-${var.org}-${var.env}-logs"
   }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "logs" {
+  bucket = aws_s3_bucket.logs.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "logs" {
+  bucket = aws_s3_bucket.logs.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_policy" "logs" {
