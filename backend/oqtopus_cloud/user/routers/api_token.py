5c46d4ee9cc5f3664c70677305462474b413c209
goralczyks <102968638+goralczyks@users.noreply.github.com>
2025-12-16 09:47:10 +0100

    API key security improvements (#320)

--- backend/oqtopus_cloud/user/routers/api_token.py
+++ backend/oqtopus_cloud/user/routers/api_token.py
@@ -1,3 +1,4 @@
+from argon2 import PasswordHasher
 from datetime import datetime, timedelta
 from secrets import token_urlsafe
 
@@ -17,7 +18,7 @@ from oqtopus_cloud.common.session import (
     get_db,
 )
 from oqtopus_cloud.user.conf import logger, tracer
-from oqtopus_cloud.user.schemas.api_token import ApiToken
+from oqtopus_cloud.user.schemas.api_token import ApiToken, ApiTokenStatus
 from oqtopus_cloud.user.schemas.errors import (
     ForbiddenErrorResponse,
     InternalServerErrorResponse,
@@ -31,51 +32,7 @@ utc = ZoneInfo("UTC")
 
 router: APIRouter = APIRouter(route_class=LoggerRouteHandler)
 
-
-@router.get(
-    "/api-token",
-    response_model=ApiToken,
-    responses={
-        403: {"model": Message},
-        404: {"model": Message},
-        500: {"model": Message},
-    },
-)
-@tracer.capture_method
-def get_api_token(
-    event: Event,
-    db: Session = Depends(get_db),
-) -> (
-    ApiToken
-    | ForbiddenErrorResponse
-    | NotFoundErrorResponse
-    | InternalServerErrorResponse
-):
-    username = event.state.owner
-    logger.info(f"Get api token: {username}")
-    try:
-        # save user table (username here is the email address registered in Cognito)
-        stmt = select(User).where(User.email == username)
-        user = db.execute(stmt).scalars().first()
-        if not user or user.api_token_secret is None:
-            logger.info("User not found")
-            return NotFoundErrorResponse(message="User not found")
-        if user.userstatus == UserStatus.suspended:  # suspended status
-            logger.info("Forbidden")
-            return ForbiddenErrorResponse(message="Forbidden")
-        else:
-            logger.info("API token found")
-            apitoken_expiration = user.api_token_expiration
-            if apitoken_expiration is not None:
-                apitoken_expiration = apitoken_expiration.replace(tzinfo=utc)
-            return ApiToken(
-                api_token_secret=user.api_token_secret,
-                api_token_expiration=apitoken_expiration,
-            )
-    except Exception as e:
-        tracer.put_annotation("error", str(e))
-        logger.exception(f"Internal Server Error: {e}")
-        return InternalServerErrorResponse(message="Internal Server Error")
+ph = PasswordHasher()
 
 
 @router.post(
@@ -99,12 +56,16 @@ def create_api_token(
 ):
     username = event.state.owner
     logger.info(f"Get api token for {username}")
-    # generate api token
-    api_token_secret = token_urlsafe(15)
-    api_token_expiration = datetime.now(utc).replace(
-        second=0, microsecond=0
-    ) + timedelta(days=90)
+
     try:
+        # generate api token
+        api_token_id = token_urlsafe(16)
+        api_token_secret = token_urlsafe(16)
+        api_token_secret_hash = ph.hash(api_token_secret)
+        api_token_expiration = datetime.now(utc).replace(
+            second=0, microsecond=0
+        ) + timedelta(days=90)
+
         # save api token to users table (username here is the email address registered in Cognito)
         stmt = select(User).where(User.email == username)
         user = db.execute(stmt).scalars().first()
@@ -117,11 +78,13 @@ def create_api_token(
                 message="this operation is currently unavailable because the status of user is [suspended]"
             )
         else:
-            user.api_token_secret = api_token_secret
+            user.api_token_id = api_token_id
+            user.api_token_hash = api_token_secret_hash
             user.api_token_expiration = api_token_expiration
             db.commit()
             logger.info("API token created")
             return ApiToken(
+                api_token_id=api_token_id,
                 api_token_secret=api_token_secret,
                 api_token_expiration=api_token_expiration,
             )
@@ -163,11 +126,57 @@ def delete_api_token(
             logger.info("Forbidden")
             return ForbiddenErrorResponse(message="Forbidden")
         else:
-            user.api_token_secret = None
+            user.api_token_id = None
+            user.api_token_hash = None
             user.api_token_expiration = None
             db.commit()
             logger.info("API token deleted")
             return Response(status_code=status.HTTP_200_OK)
+    except Exception as e:
+        tracer.put_annotation("error", str(e))
+        logger.exception(f"Internal Server Error: {e}")
+        return InternalServerErrorResponse(message="Internal Server Error")
+
+
+@router.get(
+    "/api-token/status",
+    response_model=ApiTokenStatus,
+    responses={
+        403: {"model": Message},
+        404: {"model": Message},
+        500: {"model": Message},
+    },
+)
+@tracer.capture_method
+def get_api_token_status(
+    event: Event,
+    db: Session = Depends(get_db),
+) -> (
+    ApiTokenStatus
+    | ForbiddenErrorResponse
+    | NotFoundErrorResponse
+    | InternalServerErrorResponse
+):
+    username = event.state.owner
+    logger.info(f"Get api token: {username}")
+    try:
+        # save user table (username here is the email address registered in Cognito)
+        stmt = select(User).where(User.email == username)
+        user = db.execute(stmt).scalars().first()
+        if not user or user.api_token_hash is None:
+            logger.info("User not found")
+            return NotFoundErrorResponse(message="User not found")
+        if user.userstatus == UserStatus.suspended:  # suspended status
+            logger.info("Forbidden")
+            return ForbiddenErrorResponse(message="Forbidden")
+        else:
+            logger.info("API token found")
+            api_token_expiration = user.api_token_expiration
+            if api_token_expiration is not None:
+                api_token_expiration = api_token_expiration.replace(tzinfo=utc)
+            return ApiTokenStatus(
+                api_token_expiration=api_token_expiration,
+            )
     except Exception as e:
         tracer.put_annotation("error", str(e))
         logger.exception(f"Internal Server Error: {e}")
