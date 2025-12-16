8909d362ba53c8ae909eb962d581db1fda3a904f
GoralczykS <Slawomir.Goralczyk@fujitsu.com>
2025-12-16 11:24:38 +0100

    feat: bug fixes

--- terraform/infrastructure/modules/s3/main.tf
+++ terraform/infrastructure/modules/s3/main.tf
@@ -19,6 +19,8 @@
 *
 */
 
+data "aws_caller_identity" "current" {}
+
 resource "aws_s3_bucket" "this" {
   bucket        = "${var.product}-${var.org}-${var.env}"
   force_destroy = var.force_destroy_bucket
