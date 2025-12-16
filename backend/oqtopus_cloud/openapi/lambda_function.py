ad8d31a6c1bb046b6c3966e3006fff7c23c6c97e
dvlpwork (Yugo Matsumoto) <dvlpwork@gmail.com>
2025-12-16 21:36:49 +0900

    feat: openapi function updated

--- backend/oqtopus_cloud/openapi/lambda_function.py
+++ backend/oqtopus_cloud/openapi/lambda_function.py
@@ -1,17 +1,11 @@
 import pathlib
 
-from fastapi import APIRouter, FastAPI
-from fastapi.responses import Response
 
-router = APIRouter()
-app: FastAPI = FastAPI()
-
-
-@router.get("/openapi.yaml")
-async def get_openapi_yaml():
+def lambda_handler(event, context):
     filepath = pathlib.Path(__file__).parent / "openapi.yaml"
     content = filepath.read_text()
-    return Response(content, media_type="application/yaml")
-
-
-app.include_router(router)
+    return {
+        "statusCode": 200,
+        "headers": {"Content-Type": "application/yaml"},
+        "body": content,
+    }
