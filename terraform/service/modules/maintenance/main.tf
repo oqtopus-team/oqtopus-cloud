14d9ee43befd6db772d3313743fdf60b9a2ea2b2
GoralczykS <Slawomir.Goralczyk@fujitsu.com>
2026-01-16 15:47:24 +0100

    feat:added missing version cleaner log group retention setting

--- terraform/service/modules/maintenance/main.tf
+++ terraform/service/modules/maintenance/main.tf
@@ -144,6 +144,11 @@ data "aws_iam_policy_document" "lambda_manager" {
   }
 }
 
+resource "aws_cloudwatch_log_group" "lambda_log_group" {
+  name              = "/aws/lambda/${var.product}-${var.org}-${var.env}-${var.identifier}"
+  retention_in_days = 14
+}
+
 resource "aws_cloudwatch_event_rule" "on_lambda_publish_version" {
   name        = "on-${var.product}-${var.org}-${var.env}-lambda-publish-version"
   description = "Trigger removal of unused lambda versions when new version is published"
