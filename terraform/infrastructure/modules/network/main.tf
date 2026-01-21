03660e976001c810db18ac079a29f4e48d4d724b
Massn <massn23579@gmail.com>
2026-01-21 16:51:56 +0900

    feat: setup S3 VPC Endpoint (#350)
    Co-authored-by: Katsujukou Kineya <74475348+katsujukou@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Naoyuki Masumoto <masumoto.naoyuki.qiqb@osaka-u.ac.jp>

--- terraform/infrastructure/modules/network/main.tf
+++ terraform/infrastructure/modules/network/main.tf
@@ -243,3 +243,19 @@ resource "aws_route_table_association" "private" {
   subnet_id      = aws_subnet.private[each.key].id
   route_table_id = aws_route_table.private[each.key].id
 }
+
+## S3 VPC Endpoint (Gateway)
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id            = aws_vpc.this.id
+  service_name      = "com.amazonaws.${var.region}.s3"
+  vpc_endpoint_type = "Gateway"
+  tags = {
+    Name = "${var.product}-${var.org}-${var.env}-s3-vpc-endpoint"
+  }
+}
+
+resource "aws_vpc_endpoint_route_table_association" "private_s3" {
+  for_each        = aws_route_table.private
+  route_table_id  = each.value.id
+  vpc_endpoint_id = aws_vpc_endpoint.s3.id
+}
